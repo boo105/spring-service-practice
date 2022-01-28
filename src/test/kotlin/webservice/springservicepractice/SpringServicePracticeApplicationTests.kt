a57d6ee17959f16059f394cd3aeae4389f68f597
boo105 <boo10500@naver.com>
2022-01-29 02:46:47 +0900

    study init

---
+++ src/test/kotlin/webservice/springservicepractice/SpringServicePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package webservice.springservicepractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringServicePracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
