47f0b75f3d066de44b87208e36be25c89ea048be
boo105 <boo10500@naver.com>
2022-02-02 21:03:01 +0900

    spring boot swagger 3.0 설정 및 공부

--- src/main/kotlin/webservice/springservicepractice/controller/HelloController.kt
+++ src/main/kotlin/webservice/springservicepractice/controller/HelloController.kt
@@ -1,5 +1,8 @@
 package webservice.springservicepractice.controller
 
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,23 +11,32 @@ import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class HelloController {
+
     @GetMapping("hello")
+    @ApiOperation(value = "hello index 페이지", notes = "hello spring return")
     fun hello(model : Model) : String {
         model.addAttribute("data", "spring!!")
         return "hello"
     }
 
     @GetMapping("hello-mvc")
+    @ApiOperation(value = "hello mvc", notes = "hello name return")
     fun helloMvc(@RequestParam("name") name : String, model : Model) : String {
         model.addAttribute("name", name)
         return "hello-template"
     }
 
     @GetMapping("hello-string")
+    @ApiOperation(value = "hello string", notes = "hello name return")
     @ResponseBody
     fun helloString(name : String) : String = "Hello $name"
 
     @GetMapping("hello-api")
+    @ApiOperation(value = "hello api", notes = "hello name return")
+    @ApiResponses( value = [
+        ApiResponse(code = 200, message = "ok"),
+        ApiResponse(code = 404, message = "page not found!")
+    ])
     @ResponseBody
     fun helloApi(@RequestParam("name") name : String) : Hello {
         val hello = Hello(name)
---
+++ src/test/kotlin/webservice/springservicepractice/docs/SwaggerConfig.kt
@@ -0,0 +1,48 @@
+package webservice.springservicepractice.docs
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    // https://velog.io/@borab/Spring-boot-Swagger-%EC%84%A4%EC%A0%95-gradle
+    // apis는 일단 뭐가 뭔지 모르겠음.
+    @Bean
+    fun restAPI() : Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())    // 모든 RequestMapping URI 추출
+            // .apis(RequestHandlerSelectors.basePackage("com."))   // 패키지 기준 추출
+            // .paths(PathSelectors.ant("/v2/**")) // 경로 패턴 URI만 추출
+            .paths(PathSelectors.any())
+            .build()
+            // .useDefaultResponseMessages(false)  // Response 응답 메시지 디폴트값 적용 X
+    }
+
+    private fun apiInfo() : ApiInfo {
+        return ApiInfoBuilder()
+            .title("민호의 Spring boot Rest API 공부")
+            .version("1.0.0")
+            .termsOfServiceUrl("서비스 약관 url")
+            .license("MIT")
+            .licenseUrl("http://license.com")
+            .description("Spring boot swagger api 구현 공부용입니다.")
+            .contact(
+                Contact(
+                    "Contect us",
+                    "연락할 주소",
+                    "이메일")
+            )
+            .build()
+    }
+}
