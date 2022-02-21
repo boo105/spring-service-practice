25a4d8b1594f16f4ba05c96fb5d09f80c0b624eb
boo105 <boo10500@naver.com>
2022-02-21 22:08:06 +0900

    자바와 코틀린 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation ("io.springfox:springfox-boot-starter:3.0.0")
-	// implementation("io.springfox:springfox-swagger2:2.9.2")
-	// implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
---
+++ src/main/kotlin/webservice/springservicepractice/controller/Test.kt
@@ -0,0 +1,13 @@
+package webservice.springservicepractice.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class Test {
+//    @GetMapping("/")
+//    fun home(): String? {
+//        println("test !!!")
+//        return "home"
+//    }
+}
